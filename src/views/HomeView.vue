8d4910b7f5d5e3bd03e86cf74db4a46f498588cb
kurtthezombie <kurt.desmond619@gmail.com>
2025-01-13 09:37:07 +0800

    Added partial projects section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,9 @@
   <main class="bg-gray-900 text-white scroll-smooth flex flex-col justify-center">
     <section id="landing">
       <div class="flex flex-row gap-x-20 justify-center items-center h-screen gap-y-10">
-        <div class="flex flex-col gap-y-5 items-start">
+        <div class="flex flex-col gap-y-3 items-start">
           <h1 class="text-5xl">Hi, I'm <span class="text-purple-300">Kurt Desmond Cabaluna</span></h1>
+          <h1 class="text-xl">Web Developer</h1>
           <div class="flex flex-row justify-start gap-x-10">
             <a href="#about" class="pb-2 border-b-2 border-transparent hover:border-purple-400 hover:text-purple-500 transition duration-200">About</a>
             <a href="#projects" class="pb-2 border-b-2 border-transparent hover:border-purple-400 hover:text-purple-00 transition duration-200">Projects</a>
@@ -31,14 +32,59 @@
           the dynamic field of technology.
         </p>
       </div>
+      <div class="flex flex-col items-center gap-y-5 mb-5">
+        <div class="">
+          <h3 class="text-4xl mb-5">Skills</h3>
+        </div>
+        <div class="flex flex-row justify-center gap-x-5">
+          <p class="p-3 bg-gray-300 rounded-lg text-gray-700 font-semibold hover:shadow-teal-600 hover:shadow-md hover:scale-110 transition duration-200 cursor-pointer">Laravel</p>
+          <p class="p-3 bg-gray-300 rounded-lg text-gray-700 font-semibold hover:shadow-teal-600 hover:shadow-md hover:scale-110 transition duration-200 cursor-pointer">Vue.js</p>
+          <p class="p-3 bg-gray-300 rounded-lg text-gray-700 font-semibold hover:shadow-teal-600 hover:shadow-md hover:scale-110 transition duration-200 cursor-pointer">.NET</p>
+          <p class="p-3 bg-gray-300 rounded-lg text-gray-700 font-semibold hover:shadow-teal-600 hover:shadow-md hover:scale-110 transition duration-200 cursor-pointer">MySQL</p>
+        </div>
+        <div class="flex flex-row justify-center gap-x-5">
+          <p class="p-3 bg-gray-300 rounded-lg text-gray-700 font-semibold hover:shadow-teal-600 hover:shadow-md hover:scale-110 transition duration-200 cursor-pointer">MSSQL</p>
+          <p class="p-3 bg-gray-300 rounded-lg text-gray-700 font-semibold hover:shadow-teal-600 hover:shadow-md hover:scale-110 transition duration-200 cursor-pointer">Tailwind</p>
+          <p class="p-3 bg-gray-300 rounded-lg text-gray-700 font-semibold hover:shadow-teal-600 hover:shadow-md hover:scale-110 transition duration-200 cursor-pointer">Python</p>
+          <p class="p-3 bg-gray-300 rounded-lg text-gray-700 font-semibold hover:shadow-teal-600 hover:shadow-md hover:scale-110 transition duration-200 cursor-pointer">C#</p>
+        </div>
+      </div>
       <div>
-        <a class="rounded-lg bg-purple-800 hover:bg-purple-600 p-3 px-3 hover:shadow-lg hover:shadow-emerald-800 transition duration-500 ease-in-out" href="/Resume.pdf" download>Download Resume</a>
+        <a class="rounded-lg bg-purple-800 hover:bg-purple-600 p-3 px-3 hover:shadow-lg hover:shadow-emerald-800 transition duration-500 ease-in-out" 
+        href="/Resume.pdf" target="_blank">View Resume</a>
       </div>
     </section>
     
     <section id="projects" class="mb-32">
       <div class="flex justify-center">
         <h1 class="text-5xl">Projects</h1>
+      </div>
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10 mt-10 w-1/2 mx-auto">
+        <div class="project-card p-5 bg-gray-800 rounded-lg shadow-lg hover:shadow-purple-800 hover:scale-105 cursor-pointer transition duration-200">
+          <img src="../assets/project1.png" alt="Project 1" class="w-full h-auto rounded-lg mb-4">
+          <h2 class="text-2xl mb-3">Student Task Calendar</h2>
+          <p class="mb-3">A task calendar to keep track of to-dos and deadlines</p>
+          <a href="https://github.com/kurtthezombie/task-calendar" target="_blank" class="hover:text-purple-400"><i class="fas fa-code-branch"></i> Repository</a>
+        </div>
+        <div class="project-card p-5 bg-gray-800 rounded-lg shadow-lg hover:shadow-purple-800 hover:scale-105 cursor-pointer transition duration-200">
+          <img src="../assets/project2.png" alt="Project 2" class="w-full h-auto rounded-lg mb-4">
+          <h2 class="text-2xl mb-3">WheelsOnDeals</h2>
+          <p class="mb-3">A simple car showcasing site using .NET</p>
+          <a href="https://github.com/kurtthezombie/wheels-on-deals" target="_blank" class="hover:text-purple-400"><i class="fas fa-code-branch"></i> Repository</a>
+        </div>
+        <div class="project-card p-5 bg-gray-800 rounded-lg shadow-lg hover:shadow-purple-800 hover:scale-105 cursor-pointer transition duration-200">
+          <img src="../assets/project3.png" alt="Project 3" class="w-full h-auto rounded-lg mb-4">
+          <h2 class="text-2xl mb-3">ForumsUnknown</h2>
+          <p class="mb-3">A forum site for the creepy and eerie stuff.</p>
+          <a href="https://github.com/kurtthezombie/forums-unknown-mvc" target="_blank" class="hover:text-purple-400"><i class="fas fa-code-branch"></i> Repository</a>
+        </div>
+        <!-- Add more project cards as needed -->
+        <div class="project-card p-5 bg-gray-800 rounded-lg shadow-lg hover:shadow-purple-800 hover:scale-105 cursor-pointer transition duration-200">
+          <img src="../assets/project4.png" alt="Project 4" class="w-full h-auto rounded-lg mb-4">
+          <h2 class="text-2xl mb-3">Nothing</h2>
+          <p class="mb-3">A site that tracks how long you have been doing nothing.</p>
+          <a href="https://nothing-ktz.vercel.app/" target="_blank" class="hover:text-purple-400"><i class="fas fa-link"></i> Link</a>
+        </div>
       </div>
     </section>
 
